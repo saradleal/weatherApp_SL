fbd69fb7bcfc7349d1e938d454d7827c5fdf6afb
saradleal <64803121+saradleal@users.noreply.github.com>
2020-06-27 16:53:08 +0100

    Add Current Time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let tempElement = document.querySelector("#temp");
   let temperature = Math.round(response.data.main.temp);
@@ -9,6 +32,8 @@ function displayTemperature(response) {
   windElement.innerHTML = `${windInfo} km/h`;
   let descriptElement = document.querySelector("#description");
   descriptElement.innerHTML = `${response.data.weather[0].description}`;
+  let currentDate = document.querySelector("#date");
+  currentDate.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let city = "Lisbon";
